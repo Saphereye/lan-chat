e41c787513d41a341f3dca08ac862ea07d9b8cb2
Saphereye <adarshdas950@gmail.com>
2023-09-29 07:49:46 +0530

    feat: Added `get-ip` method

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 Usage
 ---
-1. Run `lan-chat server <server-ip>` on your server machine.
-2. Use `lan-chat client <server-ip>` to connect to the server
-
-Chat commands
----
-1. `/quit` to exit the client application
+1. Run `lan-chat server --server-ip <server-ip>` to run the server.
+2. Use `lan-chat client --server-ip <server-ip>` to connect to the server
+3. Use `lan-chat get-ip` to use the Ifv4Addr ip and use that in the above commands
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,12 @@ use std::net::TcpStream;
 use std::sync::{Arc, Mutex};
 use std::thread;
 
+extern crate clap;
+use clap::{App, Arg};
+
+extern crate if_addrs;
+use if_addrs::get_if_addrs;
+
 #[derive(Clone)]
 struct Server {
     clients: Arc<Mutex<Vec<(TcpStream, String)>>>,
@@ -65,19 +71,44 @@ impl Server {
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let matches = App::new("Lan Chat")
+        .version("1.0")
+        .author("Your Name")
+        .about("A LAN chat application")
+        .subcommand(
+            App::new("server").about("Run as server").arg(
+                Arg::with_name("server-ip")
+                    .long("server-ip")
+                    .takes_value(true),
+            ),
+        )
+        .subcommand(
+            App::new("client").about("Run as server").arg(
+                Arg::with_name("server-ip")
+                    .long("server-ip")
+                    .takes_value(true),
+            ),
+        )
+        .subcommand(App::new("get-ip").about("Get your local IP address"))
+        .get_matches();
 
-    if args.len() < 3 {
-        println!("Usage: lan-chat <server/client> <server_ip>");
-        return;
-    }
-
-    let server_ip = &args[2];
-
-    match args[1].as_str() {
-        "server" => run_server(server_ip),
-        "client" => run_client(server_ip),
-        _ => println!("Invalid argument"),
+    if let Some(server_matches) = matches.subcommand_matches("server") {
+        // User wants to start the server
+        let server_ip = server_matches.value_of("server-ip").unwrap_or("127.0.0.1");
+        run_server(server_ip);
+    } else if let Some(server_matches) = matches.subcommand_matches("client") {
+        // User wants to start the server
+        let server_ip = server_matches.value_of("server-ip").unwrap_or("127.0.0.1");
+        run_client(server_ip);
+    } else if matches.subcommand_matches("get-ip").is_some() {
+        // User wants to get their local IP address
+        if let Ok(interfaces) = get_if_addrs() {
+            println!("{:#?}", interfaces);
+        } else {
+            println!("Failed to retrieve network interface information.");
+        }
+    } else {
+        println!("Usage: lan-chat <server/get-ip> or lan-chat get-ip");
     }
 }
 
